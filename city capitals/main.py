3338efac1c735a10518e79972071bb827f84dc50
WitherredAway <sdssouvic99@gmail.com>
2023-01-15 19:47:44 +0530

    7th program: city capitals

---
+++ city capitals/main.py
@@ -0,0 +1,17 @@
+"""Program to view capital of a city from a dictionary"""
+
+indian_cities = {
+    "Andhra Pradesh": "Amaravati",
+    "Arunachal Pradesh": "Itanagar",
+    "Assam": "Dispur",
+    "Bihar": "Patna",
+    "Chhattisgarh": "Raipur",
+    "Goa": "Panaji",
+    "Gujarat": "Gandhinagar",
+    "Haryana": "Chandigarh",
+    "Himachal Pradesh": "Shimla",
+    "Jharkhand": "Ranchi"
+}
+
+city = input("See capital of a city: ")
+print(indian_cities.get(city.capitalize()))
